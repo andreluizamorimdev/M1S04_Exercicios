b3ad81991ac6cbaf4f1424953351a8412013d003
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-02-14 19:52:56 -0300

    Resolução do exercicio 02

---
+++ Exercicio02/assets/scripts/main.js
@@ -0,0 +1,7 @@
+/* M1S04 - Exercicio 02
+    Declare uma variável e atribua um número a ela, em seguida, exiba seu valor no console.
+    Atribua o valor a variável usando o window.prompt();
+*/
+let numero = Number(prompt("Digite um numero"));
+
+console.log(numero);
